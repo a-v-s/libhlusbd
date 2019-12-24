35f5eb5fc2addca92eb6f56fcdb449de95500515
André van Schoubroeck <andre@philosopher.it>
2019-12-24 15:29:02 +0100

    Moving demo code to demo

--- usbd.c
+++ usbd.c
@@ -16,21 +16,7 @@
 #include "ConvertUTF/ConvertUTF.h"
 #endif
 
-// Some temprary stuff to test buffers and callbacks
-uint8_t temp_recv_buffer[64];
-void transfer_in_complete(usbd_handle_t *handle, uint8_t epnum, void *data,
-		size_t size) {
 
-}
-
-void transfer_out_complete(usbd_handle_t *handle, uint8_t epnum, void *data,
-		size_t size) {
-
-	// This is a test to reply the data increased by 1;
-	((uint8_t*) (data))[0]++;
-	usbd_transmit(handle, 0x80 | epnum, data, size);
-
-}
 
 //
 void usbd_add_endpoint_in(usbd_handle_t *handle, uint8_t config, uint8_t epnum,
@@ -77,52 +63,7 @@ void usbd_add_endpoint_out(usbd_handle_t *handle, uint8_t config, uint8_t epnum,
 	handle->ep_out[0x7F & epnum].cb = cb;
 
 }
-void usbd_demo_setup_descriptors(usbd_handle_t *handle) {
-	handle->descriptor_device = add_descriptor(handle,
-			sizeof(usb_descriptor_device_t));
-	handle->descriptor_device->bDescriptorType = USB_DT_DEVICE;
-	handle->descriptor_device->bMaxPacketSize0 = 64;
-	handle->descriptor_device->bNumConfigurations = 1;
-	handle->descriptor_device->bcdUSB = 0x0100;
-	handle->descriptor_device->idVendor = 0xdead;
-	handle->descriptor_device->idProduct = 0xbeef;
 
-	handle->descriptor_device->iManufacturer = 1;
-	handle->descriptor_device->iProduct = 2;
-
-	handle->descriptor_configuration[0] = add_descriptor(handle,
-			sizeof(usb_descriptor_configuration_t));
-	handle->descriptor_configuration[0]->wTotalLength =
-			handle->descriptor_configuration[0]->bLength;
-	handle->descriptor_configuration[0]->bDescriptorType = USB_DT_CONFIGURATION;
-	handle->descriptor_configuration[0]->bConfigurationValue = 1;
-	handle->descriptor_configuration[0]->bMaxPower = 10;
-	handle->descriptor_configuration[0]->bmAttributes = 0x80;
-	handle->descriptor_configuration[0]->bNumInterfaces = 1;
-
-	// TODO Make function to add interfaces (and altsettings)
-
-	usb_descriptor_interface_t *iface = add_descriptor(handle,
-			sizeof(usb_descriptor_configuration_t));
-	handle->descriptor_configuration[0]->wTotalLength += iface->bLength;
-	iface->bDescriptorType = USB_DT_INTERFACE;
-	iface->bInterfaceProtocol = 0xff;
-	iface->bNumEndpoints = 2;
-	iface->bInterfaceNumber = 0;
-	iface->bAlternateSetting = 0;
-
-	usbd_add_endpoint_in(handle, 1, 1, USB_EP_ATTR_TYPE_INTERRUPT, 64, 1,
-			(usbd_transfer_cb_f) &transfer_in_complete);
-	usbd_add_endpoint_out(handle, 1, 1, USB_EP_ATTR_TYPE_BULK, 64, 1,
-			temp_recv_buffer, 64, (usbd_transfer_cb_f) &transfer_out_complete);
-
-	// Be sure to save the file as UTF-8. ;)
-	handle->descriptor_string[1] = add_string_descriptor_utf8(handle, u8"🐈Blaat!");
-
-	// The u"string" prefix encodes it as UTF16 from the start
-	handle->descriptor_string[2] = add_string_descriptor_utf16(handle, u"Schaap!🐼");
-
-}
 
 usbd_handler_result_t usbd_handle_get_descriptor_request(usbd_handle_t *handle,
 		usb_setuprequest_t *req, void **buf, size_t *size) {
@@ -292,7 +233,12 @@ usbd_handler_result_t usbd_handle_standard_device_request(usbd_handle_t *handle,
 			// TODO: Callbacks when configuration changes
 
 			for (int i = 0; i < USBD_ENDPOINTS_COUNT; i++) {
+				// TODO CONFIGURE ENDPOINTS
 
+				// THIS IS A TEMPORARY SOLUTION
+				// Open all endpoints in each direction
+				usbd_ep_open(handle,i,64,2);
+				usbd_ep_open(handle,0x80|i,64,2);
 			}
 
 			return RESULT_HANDLED;
